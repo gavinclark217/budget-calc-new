7e1a331af53bfdb9a0dcd214a2d4b4dbcd68173e
Gavin Clark <gavinclark217@gmail.com>
2024-03-13 15:36:24 -0700

    temp

--- chart.js
+++ chart.js
@@ -4,15 +4,15 @@ import Chart from 'chart.js/auto'
 
 (async function () {
 
-    let spending = 0;
-    spending[0] = document.getElementById('Housing').value;
-    spending[1] = document.getElementById('Utilities').value;
-    spending[2] = document.getElementById('Transportation').value;
-    spending[3] = document.getElementById('Food').value;
-    spending[4] = document.getElementById('Debt').value;
-    spending[5] = document.getElementById('Clothing').value;
-    spending[6] = document.getElementById('Entertainment').value;
-    spending[7] = document.getElementById('Savings').value;
+    // let spending = 0;
+    // spending[0] = document.getElementById('Housing').value;
+    // spending[1] = document.getElementById('Utilities').value;
+    // spending[2] = document.getElementById('Transportation').value;
+    // spending[3] = document.getElementById('Food').value;
+    // spending[4] = document.getElementById('Debt').value;
+    // spending[5] = document.getElementById('Clothing').value;
+    // spending[6] = document.getElementById('Entertainment').value;
+    // spending[7] = document.getElementById('Savings').value;
 
     let data = {
         labels: [
@@ -27,7 +27,7 @@ import Chart from 'chart.js/auto'
         ],
         datasets: [{
             label: 'My First Dataset',
-            data: [spending[0], spending[1], spending[2], spending[3], spending[4], spending[5], spending[6], spending[7]],
+            data: [2000, 380, 830, 322, 0, 300, 2400, 1000],
             backgroundColor: [
                 'rgb(255, 0, 0)',
                 'rgb(0, 255, 0)',
